845c8f8ec6c67eac238100993c970f32787abe61
demo <praveen98845kumar@gmail.com>
2026-02-13 21:20:34 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,27 +2,47 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
+
+
   return (
-    
-    <nav className="navbar navbar-expand-lg hirex-navbar px-4">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg hirex-navbar">
+      <div className="container-fluid px-3">
 
-        {/* Logo */}
-        <a className="navbar-brand d-flex align-items-center" href="#">
-          <img src={assets.logo} alt="HireX" className="hirex-logo" />
-        </a>
+         {/* Logo */}
+        <a className="navbar-brand " href="#">
+           <img src={assets.logo} alt="HireX" className="hirex-logo" />
+         </a>
+        {/* MOBILE TOGGLE BUTTON */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#hirexNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        {/* Right Buttons */}
-        <div className="d-flex gap-2">
-          <button className="btn hirex-login"><i className="bi bi-box-arrow-in-right me-2"></i>Login</button>
-          <button className="btn hirex-recruiter">I'm Recruiter</button>
+        {/* COLLAPSE AREA */}
+        <div className="collapse navbar-collapse" id="hirexNavbar">
+
+          {/* Right side buttons */}
+          <div className="ms-auto d-flex gap-2 mt-3 mt-lg-0">
+
+            <button className="btn hirex-login">
+              <i className="bi bi-box-arrow-in-right me-2"></i>
+              Login
+            </button>
+
+            <button className="btn hirex-recruiter">
+              I'm Recruiter
+            </button>
+
+          </div>
         </div>
 
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
-
-
+export default Navbar;

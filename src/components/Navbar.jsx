4f687702b6cec1c2fd5e2f1a120daa12551430ec
demo <praveen98845kumar@gmail.com>
2026-02-13 20:58:55 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,26 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
-            <img src={assets.logo}  className="img-fluid" style={{ width: "150px" }}/>
+    
+    <nav className="navbar navbar-expand-lg hirex-navbar px-4">
+      <div className="container-fluid">
+
+        {/* Logo */}
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img src={assets.logo} alt="HireX" className="hirex-logo" />
+        </a>
+
+        {/* Right Buttons */}
+        <div className="d-flex gap-2">
+          <button className="btn hirex-login"><i className="bi bi-box-arrow-in-right me-2"></i>Login</button>
+          <button className="btn hirex-recruiter">I'm Recruiter</button>
         </div>
-        <div>
-          <button  ><i className="bi bi-door-open"></i> Recruiter login </button>
-           <button><i className="bi bi-box-arrow-in-left"></i> Login</button>
-        </div>
-        
-    </div>
+
+      </div>
+    </nav>
   )
 }
 
 export default Navbar
+
+

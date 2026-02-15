f2182f32317d05cb10c9ad3196dae6957f96204a
demo <praveen98845kumar@gmail.com>
2026-02-15 12:15:50 +0530

    logic nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,89 @@
 
+// import React from "react";
+// import { assets } from "../assets/assets";
+// import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+
+// import { Link, NavLink } from "react-router-dom";
+// const Navbar = () => {
+
+//   // Clerk hooks
+//   const { openSignIn } = useClerk();
+//   const { user } = useUser();
+
+//   return (
+//     <nav className="navbar navbar-expand-lg hirex-navbar">
+//       <div className="container-fluid px-3">
+
+//         {/* ===== LOGO ===== */}
+//         <a className="navbar-brand" href="#">
+//           <img
+//             src={assets.logo}
+//             alt="HireX"
+//             className="hirex-logo"
+//           />
+//         </a>
+
+//         {/* ===== MOBILE TOGGLE ===== */}
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#hirexNavbar"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+
+//         {/* ===== COLLAPSE AREA ===== */}
+//         <div className="collapse navbar-collapse" id="hirexNavbar">
+
+//           {/* ===== RIGHT SIDE ===== */}
+//           <div className="ms-auto d-flex gap-2 mt-3 mt-lg-0 align-items-center">
+
+//             {user ? (
+//               <>
+//                 {/* Recruiter Button */}
+//                 <button className="btn hirex-recruiter">
+//                   I'm Recruiter
+//                 </button>
+
+//                 {/* Clerk User Profile */}
+//                 <UserButton afterSignOutUrl="/" />
+//               </>
+//             ) : (
+//               <>
+//                 {/* Login Button */}
+//                 <button
+//                   onClick={() => openSignIn()}
+//                   className="btn hirex-login"
+//                 >
+//                   <i className="bi bi-box-arrow-in-right me-2"></i>
+//                   Login
+//                 </button>
+
+//  <Link to ="/recruiter/login"> <button className="btn hirex-recruiter">
+//                   I'm Recruiter
+//                 </button></Link>  
+               
+
+//               </>
+//             )}
+
+//           </div>
+//         </div>
+
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
 import React from "react";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
-import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
 
-  // Clerk hooks
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
@@ -15,13 +92,13 @@ const Navbar = () => {
       <div className="container-fluid px-3">
 
         {/* ===== LOGO ===== */}
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           <img
             src={assets.logo}
             alt="HireX"
             className="hirex-logo"
           />
-        </a>
+        </Link>
 
         {/* ===== MOBILE TOGGLE ===== */}
         <button
@@ -33,25 +110,20 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* ===== COLLAPSE AREA ===== */}
+        {/* ===== COLLAPSE ===== */}
         <div className="collapse navbar-collapse" id="hirexNavbar">
 
-          {/* ===== RIGHT SIDE ===== */}
           <div className="ms-auto d-flex gap-2 mt-3 mt-lg-0 align-items-center">
 
+            {/* ================= LOGGED IN ================= */}
             {user ? (
               <>
-                {/* Recruiter Button */}
-                <button className="btn hirex-recruiter">
-                  I'm Recruiter
-                </button>
-
-                {/* Clerk User Profile */}
+                {/* ✅ ONLY USER PROFILE */}
                 <UserButton afterSignOutUrl="/" />
               </>
             ) : (
               <>
-                {/* Login Button */}
+                {/* ✅ LOGIN BUTTON */}
                 <button
                   onClick={() => openSignIn()}
                   className="btn hirex-login"
@@ -60,11 +132,12 @@ const Navbar = () => {
                   Login
                 </button>
 
- <Link to ="/recruiter/login"> <button className="btn hirex-recruiter">
-                  I'm Recruiter
-                </button></Link>  
-               
-
+                {/* ✅ SHOW RECRUITER ONLY WHEN LOGGED OUT */}
+                <Link to="/recruiter/login">
+                  <button className="btn hirex-recruiter">
+                    I'm Recruiter
+                  </button>
+                </Link>
               </>
             )}
 
